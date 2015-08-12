adc8759435c4315f2e2790a35be81eb38ab6e500
Jonathan Vicente <jwv@mmstudio.pt>
2015-08-12 18:05:09 +0100

    exclude some files

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "brew.sh" --exclude "dotfiles.sublime-project" --exclude "dotfiles.sublime-workspace" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
